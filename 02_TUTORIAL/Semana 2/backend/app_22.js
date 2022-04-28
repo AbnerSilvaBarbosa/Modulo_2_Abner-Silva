e55582e2b5db8992dc9d44c97ab22ffe5a7efd0e
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2022-04-27 23:27:01 -0300

    ADD a server express for html

--- 02_TUTORIAL/Semana 2/backend/app_22.js
+++ 02_TUTORIAL/Semana 2/backend/app_22.js
@@ -1,10 +1,14 @@
 
 const express = require('express'); 
+const path = require("path");
 const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3022;
-app.use(express.static("../frontend/"));
+
+app.use("/",(req,res)=>{
+  res.render(path.join(__dirname,"index"))
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
