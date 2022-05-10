77bcf778b47dab137081124e7d461ef7d9bfddc8
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2022-05-10 14:06:23 -0300

    Tutorial da semana 4 realizado , e entregavel da semana 4

---
+++ 03_AUT_EST_ENTREGA/Semana 2/Curriculo_Abner/public/script.js
@@ -0,0 +1,33 @@
+var a
+
+function showText() {
+
+
+
+    
+    $("#nameHidden").append(`<img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg' />
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'/>
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' />
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg' />
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/godot/godot-original.svg' />
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg' />
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg' />
+
+
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg' />
+
+
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg' />
+
+
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/slack/slack-original.svg' />
+
+
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg' />
+
+
+    <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg' />`)
+
+        
+
+}
